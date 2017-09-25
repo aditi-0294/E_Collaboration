ccc1d40353d78cc5488778d347814227926baf20
Aditi <a.aditi0294@rediffmail.com>
2017-09-25 19:31:14 +0530

    similar to static resources file

---
+++ StuforexBackend/src/main/java/com/niit/configuration/WebConfig.java
@@ -0,0 +1,65 @@
+package com.niit.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc	// same as <mvc:annotation-driven /> - dtects @RequestMapping
+@ComponentScan(basePackages = "com.niit")	// same as <context:component-scan base-package = "com.niit" />
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+		public WebConfig() {
+			
+			System.out.println("Default Constructor of WebConfig.java");
+			System.out.println();
+		}
+	
+		/* for internal resource view resolver - not required ... */ 
+		/*
+		@Bean
+		public InternalResourceViewResolver internalResourceViewResolver() {
+		
+			InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver() ;
+			internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+			internalResourceViewResolver.setSuffix(".jsp");
+			return internalResourceViewResolver ;
+			
+		}
+		*/
+		
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+			System.out.println("WebConfig.java - addResourceHandlers()");
+			System.out.println();
+			
+			
+			// To write <mvc:resources location = "/WEB-INF/resources/" mapping = "/resources/**" />
+
+			registry.addResourceHandler("/resources/**")  
+					.addResourceLocations("/WEB-INF/resources/") ; // example : to store profile_pic
+		
+	}
+
+		
+		// for profile_pic upload - multipart file upload :
+		@Bean(name = "multipartResolver")
+		public CommonsMultipartResolver getCommonsMultipartResolver() {
+			
+			System.out.println("WebConfig.java - getCommonsMultipartResolver()");
+			System.out.println();
+			
+			CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver() ;
+			
+			commonsMultipartResolver.setMaxUploadSize(10485760); // for 10MB ( 10 * 1024 * 1024 = 10485760 bytes)
+	//		commonsMultipartResolver.setMaxInMemorySize(1048576); // for 1MB ( 1024 * 1024 = 1048576 bytes)
+			
+			return commonsMultipartResolver ;
+		}
+}
